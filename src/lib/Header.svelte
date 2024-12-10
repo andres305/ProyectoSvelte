90d127b4c2fe1ebaec2ab584c032e99436a4b80b
Andres Jimenez <andresjimber@gmail.com>
2024-12-10 13:10:13 +0100

    Creacion del header y el nav

---
+++ src/lib/Header.svelte
@@ -0,0 +1,65 @@
+<script>
+</script>
+
+<div class="header">
+    <div class="logo">
+        <img src="../../logo-mercapp.png" alt="Cart Icon" width="70" />
+    </div>
+
+    <div class="buscador">
+        <input type="text" placeholder="Buscar..." />
+    </div>
+
+    <div class="icons">
+        <button class="icon">
+            <img src="../../likes.png" alt="Heart Icon" />
+        </button>
+        
+        <button class="icon">
+            <img src="../../perfil.png" alt="Profile Icon" />
+        </button>
+    </div>
+</div>
+
+<style>
+    .header {
+        margin: 0;
+        display: flex;
+        align-items: center;
+        background-color: #baf7a1;
+        padding: 20px;
+    }
+
+    .logo {
+        margin-right: 10px;
+    }
+
+    .buscador {
+        flex-grow: 1;
+        display: flex;
+        justify-content: center;
+    }
+
+    .buscador input {
+        width: 80%;
+        padding: 8px;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+        background: rgb(227, 227, 227);
+    }
+
+    .icons {
+        display: flex;
+        align-items: center;
+        gap: 15px;
+    }
+
+    .icon {
+        background: none;
+        border: 0;
+    }
+
+    .icon img {
+        width: 50px; 
+    }
+</style>
---
+++ src/lib/Nav.svelte
@@ -0,0 +1,61 @@
+<script>
+</script>
+
+<div class="nav">
+    <div class="menu-icono">
+        <img src="../../menu.png" alt="Menu Icon" />
+    </div>
+
+    <div class="grupo-botones">
+        <button>Muebles</button>
+        <button>Videojuegos</button>
+        <button>Coches</button>
+        <button>Moda</button>
+        <button>Tecnología</button>
+        <button>Deportes</button>
+    </div>
+</div>
+
+<style>
+    .nav {
+        display: flex;
+        align-items: center;
+        background-color: #E2FFED; 
+        padding: 10px;
+        width: 100%; 
+        box-sizing: border-box;
+        gap: 10px;
+    }
+
+    .menu-icono {
+        display: flex;
+        align-items: center;
+    }
+
+    .menu-icono img {
+        width: 24px; 
+        height: 24px;
+    }
+
+    .grupo-botones {
+        display: flex;
+        width: 100%;
+        justify-content: space-evenly;
+        gap: 10px; 
+    }
+
+    .grupo-botones button {
+        background-color: #d6f8c4;
+        border: none;
+        border-radius: 5px;
+        padding: 8px 12px;
+        cursor: pointer;
+        font-size: 14px;
+        box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.2); 
+    }
+
+    .grupo-botones button:hover {
+        background-color: #c3eab5; 
+    }
+    
+</style>
